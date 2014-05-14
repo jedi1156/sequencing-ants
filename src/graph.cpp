5c5fc0a23ed81c0753a1f1954f4ceceb0f32bcc6
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-05-14 12:06:46 +0200

    scaffold for graph constr/destr

---
+++ src/graph.cpp
@@ -0,0 +1,16 @@
+#include "graph.hpp"
+
+Graph::Graph(string filename) {
+  // open file
+
+  // load nodes
+  // create matrix
+
+  // close file
+}
+
+Graph::~Graph() {
+  // deallocate nodes
+  // deallocate edges
+  // deallocate matrix
+}
