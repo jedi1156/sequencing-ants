26cb3ac50c39521c00dc1e8029e371f500990c20
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-05-14 11:59:44 +0200

    ant

---
+++ src/ant.hpp
@@ -0,0 +1,15 @@
+#ifndef _INCLUDE_ANT_HPP
+#define _INCLUDE_ANT_HPP
+
+#include "common.hpp"
+
+class Ant {
+private:
+  vector<unsigned> no_visits;
+  unsigned no_unique_visits;
+
+public:
+  Ant();
+};
+
+#endif
