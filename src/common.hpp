ba4f0894da36faf9cb5ac1502f0342a96acbcdec
Karol Lisiecki <lisiecki.karol@gmail.com>
2014-05-14 12:33:32 +0200

    merge

--- src/common.hpp
+++ src/common.hpp
@@ -3,7 +3,8 @@
 
 #include <string>
 #include <vector>
-
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 #endif
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,12 +1,27 @@
 #include "graph.hpp"
 
+using namespace std;
+
 Graph::Graph(string filename) {
-  // open file
+	// open file
+  ofstream file;
+  file.open(filename, ios:in);
 
   // load nodes
-  // create matrix
+  string s;
+  while(file >> s) {
+    nodes.push_back(new Node(s));
+  }
+
+  //  matrix
+  for (int i=0; i<nodes.size(); i++) {
+    for (int j=0; j<nodes.size(); j++) {
+
+    }
+  }
 
   // close file
+  file.close();
 }
 
 Graph::~Graph() {
