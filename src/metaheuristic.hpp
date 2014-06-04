fc8e1c03955a89170611c375875056b2bfaf3498
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-06-04 16:40:36 +0200

    Metaheuristic interface

---
+++ src/metaheuristic.hpp
@@ -0,0 +1,16 @@
+#ifndef _INCLUDE_METAHEURISTIC_HPP
+#define _INCLUDE_METAHEURISTIC_HPP
+
+class Metaheuristic {
+private:
+  bool working;
+
+public:
+  Metaheuristic() : working(true) {}
+  virtual ~Metaheuristic() {}
+  bool is_working() { return working; }
+  void terminate() { working = false; }
+  void optimize() = 0;
+};
+
+#endif
