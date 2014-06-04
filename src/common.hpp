293d220bb9b590412a57f80ae7254a1a604ec25d
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-06-04 16:44:15 +0200

    Use params and strategies

--- src/common.hpp
+++ src/common.hpp
@@ -1,6 +1,9 @@
 #ifndef _INCLUDE_COMMON_HPP
 #define _INCLUDE_COMMON_HPP
 
+#include <signal.h>
+#include <unistd.h>
+
 #include <string>
 #include <vector>
 #include <iostream>
@@ -8,6 +11,11 @@
 #include <cmath>
 #include <thread>
 #include <map>
+
+#define ALPHA 2
+#define BETA 15
+#define GAMMA 1
+#define NUMBER_OF_ANTS 10
 
 #define D 0
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,42 @@
 #include "common.hpp"
 #include "graph.hpp"
+#include "aco_parallel_strategy.hpp"
+#include "aco_sequential_strategy.hpp"
+#include "metaheuristic.hpp"
+#include "aco.hpp"
+
+#define PARALLEL true
+
+#define MAX_SOLUTION_LENGTH 100
+
+Metaheuristic *metaheuristic = NULL;
+void terminte_work(int sig) {
+  if (metaheuristic) { metaheuristic->terminate(); }
+}
 
 int main(int argc, char* argv[]) {
+  signal(SIGINT, terminte_work);
+
   if(argc == 2) {
-    Graph *g = new Graph(argv[1]);
-    if (D) { cout << *g << endl; }
+    bool parallel = PARALLEL; // TODO: read from args
 
-    ACO *colony = new ACO(g, NUMBER_OF_ANTS);
-    colony->optimize();
+    Graph *graph = new Graph(argv[1]);
+    if (D) { cout << *graph << endl; }
 
-    delete g;
-    delete colony;
+    ACOParameters params(ALPHA, BETA, GAMMA, NUMBER_OF_ANTS, MAX_SOLUTION_LENGTH);
+
+    ACOStrategy *strategy;
+    if (parallel) {
+      strategy = new ACOParallelStrategy(params);
+    } else {
+      strategy = new ACOSequentialStrategy(params);
+    }
+
+    metaheuristic = new ACO(g, strategy);
+    metaheuristic->optimize();
+
+    delete metaheuristic;
+    delete graph;
   }
   return 0;
 }
